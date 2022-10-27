95711279f1c5f647bd66b6747f1c1e004e838117
Kunal Hole <kunalhole538@gmail.com>
2022-10-27 11:03:15 +0530

    Develop Header Section

---
+++ components/HeroSection.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+
+const HeroSection = () => {
+  return (
+    <div className="w-full h-screen flex items-center ">
+      <div className=" relative w-full h-max flex items-center justify-evenly">
+        <Image
+          src="/login.svg"
+          alt="login"
+          width={600}
+          height={500}
+          className="object-contain hidden md:inline-flex"
+        />
+
+        <div className="  flex flex-col p-5 items-center space-y-20 font-semibold text-[40px]">
+          <h1 className="text-secondary text-center font-bold">
+            Let&apos;s <span className="italic text-rear">Census</span> it
+            Effectively!
+          </h1>
+          <button className="bg-secondary p-3 rounded-md text-primary  sm:text-[32px] text-[20px] hover:bg-rear hover:text-secondary hover:scale-105 transition font-bold active:bg-rear">
+            Connect Wallet
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ components/Navbar.jsx
@@ -0,0 +1,84 @@
+import Link from "next/link"
+import { MenuIcon, XIcon } from "@heroicons/react/outline"
+import { useState } from "react"
+
+const Navbar = () => {
+  const [sideNav, setSideNav] = useState(false)
+
+  const handleNav = () => {
+    setSideNav(!sideNav)
+  }
+  return (
+    <header className="fixed z-[100] w-full bg-secondary h-20">
+      <div className="flex items-center w-full h-full px-10 justify-between">
+        <Link href="/">
+          <h1 className="text-[32px] font-semibold cursor-pointer text-rear">
+            E-census
+          </h1>
+        </Link>
+
+        <div className="">
+          <ul className="hidden md:flex space-x-4 p-5 text-primary text-md">
+            <Link href="/about">
+              <li className="ml-10 uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/contact">
+              <li className="ml-10 uppercase hover:border-b">contact</li>
+            </Link>
+            <Link href="/sitemap ">
+              <li className="ml-10 uppercase hover:border-b">sitemap</li>
+            </Link>
+          </ul>
+          <div
+            onClick={handleNav}
+            className="md:hidden cursor-pointer text-primary"
+          >
+            <MenuIcon className="h-7" />
+          </div>
+        </div>
+      </div>
+      <div
+        className={
+          sideNav
+            ? "sm:hidden fixed left-0 top-0 w-full h-screen bg-black/20"
+            : ""
+        }
+      >
+        <div
+          className={
+            sideNav
+              ? "sm:hidden fixed bg-secondary w-[60%] top-0 left-0 h-screen p-10 ease-in duration-500"
+              : "sm:hidden left-[-100%] fixed top-0 p-10 ease-out duration-500"
+          }
+        >
+          <div>
+            <div className="flex items-center justify-between max-w-md min-w-fit">
+              <h1 className="text-2xl font-semibold cursor-pointer text-rear">
+                E-census
+              </h1>
+              <div className="rounded-full cursor-pointer text-primary">
+                <XIcon onClick={handleNav} className="h-6" />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5">
+            <ul className="uppercase text-primary">
+              <Link href="/about">
+                <li className="py-4 text-sm  hover:border-b">About</li>
+              </Link>
+              <Link href="/contact">
+                <li className="py-4 text-sm hover:border-b">contact</li>
+              </Link>
+              <Link href="/sitemap ">
+                <li className="py-4 text-sm  hover:border-b">sitemap</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
